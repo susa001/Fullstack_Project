5d29dc94d9350f2521c3cbd6dc5b38384421629a
susa001 <sivajaya3000@gmail.com>
2023-08-26 16:51:16 +0530

    second commit

--- src/Current.js
+++ src/Current.js
@@ -1,13 +1,30 @@
 import './App.css'
+import {useEffect, useState, useRef} from 'react'
+
 function Current()
 {
-    var a=5000;
+    const inputref=useRef();
+    const [amt,setamt]=useState(0);
+    const [bal,setbal]=useState(amt);
+    useEffect(()=>{
+        setbal(amt);
+    },[amt])
     return(
      <div className='current'>
-        <h3>CURRENT BALANCE</h3>
-        <h4>{a}</h4>
+        <div className='squ'>
+            <div className='amt'>
+                <h4>{amt}</h4>
+                <h4>{bal}</h4>
+            </div>
+        </div>
+        <div className='sal'>
+            <input type='number' required="" ref={inputref}></input>
+            <br></br>
+            <button onClick={()=>{setamt(inputref.current.value)}}>sumbit</button>
+            <br></br>
+            <input type='number' required="" onChange={(e)=>{setbal(amt-e.target.value)}}></input>
+        </div>
      </div>
     );
 }
-
 export default Current;
